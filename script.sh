84d230ef1bbe8d029f933947fc53f4e8d8d5ccda
Abbastanza <6gcjgdqh@duck.com>
2023-11-01 15:16:18 +0100

    shorten script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 # 
 # Issuer or account-name can't have whitespaces
-# TODO: apple script dialogs as functions
+# Last line of shortcuts file has to be empty
 # 
 
 keys=$(ykman list)
@@ -10,14 +10,14 @@ if [ "$keys" == "" ]; then
     exit 1
 fi
 
-# checks (a), (b) and (c) are only performed when no shortcut has been used
+# checks (a) and (b) are only performed when no shortcut has been used
 check=true
 
 # Get account input from user
 account_input=$(osascript -e 'display dialog "Enter account:" default answer "" buttons {"OK"} default button 1')
 account=${account_input:34}
 
-# Last line of shortcuts file has to be empty!
+# Check shortcut file if the user input matches a shortcut
 while read p; do
     pos_comma=$(awk -v a="$p" -v b="," 'BEGIN{print index(a,b)}')
     shortcut=${p:0:pos_comma-1}
@@ -37,29 +37,12 @@ if [ ! "$account" ] && $check; then
     exit 1
 fi
 
-full_code=$(ykman oath accounts code $account)
+code=$(ykman oath accounts code $account -s)
 
-# (b) exit if result is empty
-if [ ! "$full_code" ] && $check; then
-    osascript -e 'display dialog "Invalid account name. No matching account found. Please try again!" buttons {"OK"} default button "OK" with icon caution'
+# (b) exit if result is empty or no results have been found for the input
+if [ ! "$code" ] && $check; then
+    osascript -e 'display dialog "Invalid account name. Either no or ambivalent result. Please try again!" buttons {"OK"} default button "OK" with icon caution'
     exit 1
 fi
 
-# (c) count ':' to check if multiple results have been found for the input
-if $check; then
-    declare -i counter=0
-    for i in $(seq 0 ${#full_code});
-    do
-        char=${full_code:$i-1:1}
-        if ((counter > 1 )); then
-            osascript -e 'display dialog "Invalid account name. Multiple accounts found. Please try again!" buttons {"OK"} default button "OK" with icon caution'
-            exit 1
-        fi
-        if [ "$char" == ":" ]; then
-            ((counter++))
-        fi
-    done
-fi
-
-pos_whitespace=$(awk -v a="$full_code" -v b=" " 'BEGIN{print index(a,b)}')
-echo ${full_code:pos_whitespace+1} | pbcopy
+echo $code | pbcopy
